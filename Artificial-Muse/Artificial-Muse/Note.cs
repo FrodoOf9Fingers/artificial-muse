0170481ffc83f2c4f7a1940ea715859793f86399
Roman-Sanchez <romanfsanchez@gmail.com>
2017-02-10 12:59:19 -0700

    adding the C# Atrificial-Muse project files.

---
+++ Artificial-Muse/Artificial-Muse/Note.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artificial_Muse
+{
+    class Note: Measure
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="length">Length of the note</param>
+        /// <param name="pitch">The pitch of the note</param>
+        Note(double length, string pitch)
+        {
+            this.length = length;
+            this.pitch = pitch;
+        }
+
+        public double length
+        {
+            get;
+            set;
+        }
+
+        public string pitch
+        {
+            get;
+            set;
+        }
+    }
+}
